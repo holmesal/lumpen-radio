d5a68b01c0322f7f5a36b878a6ee8c4deb48e7f4
Josh Habdas <jhabdas@gmail.com>
2015-08-22 14:28:33 -0500

    Doc methods in RootViewController

--- iOS/Classes/RootViewController.swift
+++ iOS/Classes/RootViewController.swift
@@ -4,18 +4,23 @@ import UIKit
 
 class RootViewController: UIViewController {
   
+  // Allow background observation of remote control events
   override func canBecomeFirstResponder() -> Bool {
     return true
   }
   
   override func viewDidAppear(animated: Bool) {
     super.viewDidAppear(animated)
+    
+    // Start listening for remote control events
     self.becomeFirstResponder()
     UIApplication.sharedApplication().beginReceivingRemoteControlEvents()
   }
   
   override func viewWillDisappear(animated: Bool) {
     super.viewWillDisappear(animated)
+    
+    // Stop listening for remote control events
     self.resignFirstResponder()
     UIApplication.sharedApplication().endReceivingRemoteControlEvents()
   }
